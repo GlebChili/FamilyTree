b8996186a03a665cd20cac2c54a58643931bf1e7
Gleb Krasilich <gleb.chili@gmail.com>
2021-10-15 04:00:06 +0300

    Layout added

---
+++ FamilyTree.Web/Shared/CustomNavLink.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace FamilyTree.Web.Shared
+{
+    public partial class CustomNavLink
+    {
+        [Parameter]
+        public string? Href { get; set; }
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        [Parameter]
+        public NavLinkMatch Match { get; set; }
+    }
+}
